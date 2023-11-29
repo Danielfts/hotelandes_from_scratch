70612b90fdbf9873e3f347ba57813acd34539b1e
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2023-11-29 13:10:38 -0500

    Completed habitacion create request

--- src/main/java/edu/uniandes/hotelandes/entities/HabitacionEntity.java
+++ src/main/java/edu/uniandes/hotelandes/entities/HabitacionEntity.java
@@ -3,6 +3,7 @@ package edu.uniandes.hotelandes.entities;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -20,8 +21,10 @@ public class HabitacionEntity {
      * asdasdkjlf
      * 
      */
-    
+    @Id
     private String id;
+
+    @Indexed(unique = true)
     private String numero;
     private TipoHabitacionEntity tipoHabitacion;
 
